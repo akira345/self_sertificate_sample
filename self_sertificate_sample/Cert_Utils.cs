340767b1a8f7b36094a452654d96ea3823693d13
akira345 <g.akira345@gmail.com>
2017-08-31 22:18:50 +0900

    サンプルをコミット

---
+++ self_sertificate_sample/Cert_Utils.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.X509;
+using System.Security.Cryptography.X509Certificates;
+
+// http://stackoverflow.com/questions/36712679/bouncy-castles-x509v3certificategenerator-setsignaturealgorithm-marked-obsolete
+// をベースに細々修正したもの。
+
+namespace self_sertificate_sample
+{
+    class Cert_Utils
+    {
+        public static X509Certificate2 CreateSelfSignedCertificateBasedOnCertificateAuthorityPrivateKey(string subjectName, string issuerName, AsymmetricKeyParameter issuerPrivKey)
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+            ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA512WITHRSA", issuerPrivKey, random);
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            certificateGenerator.AddExtension(X509Extensions.ExtendedKeyUsage.Id, false, new ExtendedKeyUsage(KeyPurposeID.IdKPServerAuth));
+            //CA=FALSEでcritical属性付与
+            certificateGenerator.AddExtension(X509Extensions.BasicConstraints.Id, true, new BasicConstraints(false));
+            //鍵用途
+            certificateGenerator.AddExtension(X509Extensions.KeyUsage.Id, false, new KeyUsage(KeyUsage.DigitalSignature | KeyUsage.KeyEncipherment));
+
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            //const string signatureAlgorithm = "SHA512WITHRSA";
+            //certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name(subjectName);
+            X509Name issuerDN = new X509Name(issuerName);
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(2);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            var keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            var keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            //サブジェクトキー識別子
+            var subjectKeyIdentifier = new SubjectKeyIdentifier(SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(subjectKeyPair.Public));
+            certificateGenerator.AddExtension(X509Extensions.SubjectKeyIdentifier, false, subjectKeyIdentifier);
+
+            //対Chrome用にSubjectAlternativeName属性追加(CNをそのまま入れる）
+            var subjectAltName = new GeneralNames(new GeneralName(GeneralName.DnsName, subjectDN.GetValueList(X509ObjectIdentifiers.CommonName)[0].ToString()));
+            certificateGenerator.AddExtension(X509Extensions.SubjectAlternativeName, false, subjectAltName);
+
+            // selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(signatureFactory);
+
+            // correcponding private key
+            PrivateKeyInfo info = PrivateKeyInfoFactory.CreatePrivateKeyInfo(subjectKeyPair.Private);
+
+
+            // merge into X509Certificate2
+            X509Certificate2 x509 = new X509Certificate2(certificate.GetEncoded());
+
+            Asn1Sequence seq = (Asn1Sequence)Asn1Object.FromByteArray(info.ParsePrivateKey().GetDerEncoded());
+            if (seq.Count != 9)
+            {
+                //throw new PemException("malformed sequence in RSA private key");
+            }
+
+            RsaPrivateKeyStructure rsa = RsaPrivateKeyStructure.GetInstance(seq); //new RsaPrivateKeyStructure(seq);
+            RsaPrivateCrtKeyParameters rsaparams = new RsaPrivateCrtKeyParameters(
+                rsa.Modulus, rsa.PublicExponent, rsa.PrivateExponent, rsa.Prime1, rsa.Prime2, rsa.Exponent1, rsa.Exponent2, rsa.Coefficient);
+
+            x509.PrivateKey = DotNetUtilities.ToRSA(rsaparams);
+            return x509;
+
+        }
+
+
+        public static X509Certificate2 CreateCertificateAuthorityCertificate(string subjectName, ref AsymmetricKeyParameter CaPrivateKey)
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            //const string signatureAlgorithm = "SHA256WithRSA";
+            //certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name(subjectName);
+            X509Name issuerDN = subjectDN;
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(2);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            KeyGenerationParameters keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            RsaKeyPairGenerator keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+
+            //CA=TRUEでcritical属性付与  
+            certificateGenerator.AddExtension(X509Extensions.BasicConstraints.Id, true, new BasicConstraints(true));
+            //鍵用途
+            certificateGenerator.AddExtension(X509Extensions.KeyUsage.Id, true, new KeyUsage(KeyUsage.KeyCertSign | KeyUsage.CrlSign));
+            //サブジェクト鍵識別子
+            var subjectKeyIdentifier = new SubjectKeyIdentifier(SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(subjectKeyPair.Public));
+            certificateGenerator.AddExtension(X509Extensions.SubjectKeyIdentifier, false, subjectKeyIdentifier);
+
+            //機関鍵識別子
+            var authorityKeyIdentifier = new AuthorityKeyIdentifier(
+                SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(subjectKeyPair.Public),
+                new GeneralNames(new GeneralName(issuerDN)),
+                serialNumber);
+            certificateGenerator.AddExtension(X509Extensions.AuthorityKeyIdentifier.Id, false, authorityKeyIdentifier);
+            // Generating the Certificate
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+            ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA512WITHRSA", issuerKeyPair.Private, random);
+            // selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(signatureFactory);
+            X509Certificate2 x509 = new X509Certificate2(certificate.GetEncoded());
+
+            CaPrivateKey = issuerKeyPair.Private;
+            // CA秘密鍵チェック
+            PrivateKeyInfo info = PrivateKeyInfoFactory.CreatePrivateKeyInfo(subjectKeyPair.Private);
+
+            Asn1Sequence seq = (Asn1Sequence)Asn1Object.FromByteArray(info.ParsePrivateKey().GetDerEncoded());
+            if (seq.Count != 9)
+            {
+                //throw new PemException("malformed sequence in RSA private key");
+            }
+            //作成したキーペアの秘密鍵をX509の秘密鍵へ形式変換する
+            RsaPrivateKeyStructure rsa = RsaPrivateKeyStructure.GetInstance(seq);
+            RsaPrivateCrtKeyParameters rsaparams = new RsaPrivateCrtKeyParameters(
+                rsa.Modulus, rsa.PublicExponent, rsa.PrivateExponent, rsa.Prime1, rsa.Prime2, rsa.Exponent1, rsa.Exponent2, rsa.Coefficient);
+            //変換したプライベートキーをセット
+            x509.PrivateKey = DotNetUtilities.ToRSA(rsaparams);
+
+            return x509;
+            //return issuerKeyPair.Private;
+
+        }
+        public static string GetPublickeyPemString(X509Certificate2 public_key)
+        {
+            StringBuilder str_pem = new StringBuilder();
+            str_pem.Clear();
+            str_pem.AppendLine("-----BEGIN CERTIFICATE-----");
+            str_pem.AppendLine(Convert.ToBase64String(public_key.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks));
+            str_pem.AppendLine("-----END CERTIFICATE-----");
+
+            return str_pem.ToString();
+        }
+
+    }
+}
---
+++ self_sertificate_sample/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Org.BouncyCastle.Crypto;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using Org.BouncyCastle.OpenSsl;
+
+namespace self_sertificate_sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            //サンプルなので保存先は適当
+            Generate_Self_Certificate("c:\\temp", "c:\\temp", "example.com");
+            MessageBox.Show("OK");
+        }
+        /// <summary>
+        /// オレオレ証明書を作成します。
+        /// </summary>
+        /// <param name="cert_path">作成したCA公開鍵及び証明書ファイルの保存先</param>
+        /// <param name="key_path">作成した証明書秘密鍵保存先</param>
+        /// <param name="hostname">作成する証明書のホスト名</param>
+        public static void Generate_Self_Certificate(string cert_path, string key_path, string hostname)
+        {
+            AsymmetricKeyParameter myCAprivateKey = null;
+
+            string ca_certificate_info = "C=JP, ST=Hiroshima, L=Hiroshima, O=, OU=, CN=CA";//認証局情報
+            string server_certificate_info = "C=JP, ST=Hiroshima, L=Hiroshima, O=, OU=, OU=CA, CN=" + hostname; //サーバ証明書情報
+
+            //認証局作成
+            Console.WriteLine("Creating CA");
+
+            X509Certificate2 certificateAuthorityCertificate = Cert_Utils.CreateCertificateAuthorityCertificate(ca_certificate_info, ref myCAprivateKey);
+
+            MessageBox.Show(certificateAuthorityCertificate.ToString(true));
+            MessageBox.Show(certificateAuthorityCertificate.ToString(false));
+
+            //CA公開鍵保存(PEM)
+            File.WriteAllText(cert_path + "\\" + hostname + ".ca", Cert_Utils.GetPublickeyPemString(certificateAuthorityCertificate));
+
+            //CA秘密鍵保存(PEM)を保存する場合
+            var ca_key_parameter = certificateAuthorityCertificate.GetRSAPrivateKey().ExportParameters(true);
+            var ca_privateKey = RsaPemMaker.GetPrivatePemString(ca_key_parameter);
+            File.WriteAllText(key_path + "\\" + hostname + ".cakey", ca_privateKey);
+
+            //作成したCAの秘密鍵を読み込む
+            myCAprivateKey = readPrivateKey(key_path + "\\" + hostname + ".cakey");
+
+            //サーバ証明書を作成し、CAの秘密鍵で署名する。
+            Console.WriteLine("Creating certificate based on CA");
+            X509Certificate2 certificate = Cert_Utils.CreateSelfSignedCertificateBasedOnCertificateAuthorityPrivateKey(server_certificate_info, ca_certificate_info, myCAprivateKey);
+
+            //サーバ証明書公開鍵保存(PEM)
+            File.WriteAllText(cert_path + "\\" + hostname + ".crt", Cert_Utils.GetPublickeyPemString(certificate));
+
+            //サーバ証明書秘密鍵保存(PEM)
+            RSAParameters key_parameter = certificate.GetRSAPrivateKey().ExportParameters(true);
+            string privateKey = RsaPemMaker.GetPrivatePemString(key_parameter);
+            File.WriteAllText(key_path + "\\" + hostname + ".key", privateKey);
+            Console.WriteLine(privateKey);
+        }
+        /// <summary>
+        /// プライベートキーを読み込みます。
+        /// </summary>
+        /// <param name = "privateKeyFileName" > PEM形式のプライベートキーファイル名 </ param >
+        /// < returns ></ returns >
+        private static AsymmetricKeyParameter readPrivateKey(string privateKeyFileName)
+        {
+            AsymmetricCipherKeyPair keyPair;
+
+            using (var reader = File.OpenText(privateKeyFileName))
+            {
+                keyPair = (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+            }
+            return keyPair.Private;
+        }
+
+    }
+}
